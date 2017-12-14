f89955d9ac50c204cef5f0bce989d4883e91d04b
lucas-soares <lucas3designer@gmail.com>
2017-12-14 19:21:41 -0200

    Adiciona layout criar oferta

---
+++ app/src/main/java/br/com/soaresdeveloper/tribarato/CriarOfertaActivity.java
@@ -0,0 +1,13 @@
+package br.com.soaresdeveloper.tribarato;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriarOfertaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_oferta);
+    }
+}
