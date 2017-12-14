4de14f64326a80da77651ad5056303d3e5d43a14
lucas-soares <lucas3designer@gmail.com>
2017-12-14 18:36:07 -0200

    Implementa OfertaAdapter

---
+++ app/src/main/java/br/com/soaresdeveloper/tribarato/adapter/OfertaAdapter.java
@@ -0,0 +1,64 @@
+package br.com.soaresdeveloper.tribarato.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.soaresdeveloper.tribarato.R;
+import br.com.soaresdeveloper.tribarato.entidades.Oferta;
+
+/**
+ * Created by Soares on 14/12/2017.
+ */
+
+public class OfertaAdapter extends ArrayAdapter<Oferta> {
+
+    public OfertaAdapter(Context context, int resource, List<Oferta> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_oferta, parent, false);
+        }
+
+        TextView titulo = (TextView) convertView.findViewById(R.id.titulo);
+        TextView autor = (TextView) convertView.findViewById(R.id.autor);
+        TextView descricao = (TextView) convertView.findViewById(R.id.descricao);
+        TextView preco = (TextView) convertView.findViewById(R.id.preco);
+        Button btnQueroIsto = (Button) convertView.findViewById(R.id.btnQueroIsto);
+
+        Oferta oferta = getItem(position);
+
+//      TODO Implementar envio de fotos nas ofertas
+//        boolean isPhoto = message.getPhotoUrl() != null;
+//        if (isPhoto) {
+//            messageTextView.setVisibility(View.GONE);
+//            photoImageView.setVisibility(View.VISIBLE);
+//            Glide.with(photoImageView.getContext())
+//                    .load(message.getPhotoUrl())
+//                    .into(photoImageView);
+//        }
+
+        titulo.setText(oferta.getTitulo());
+        autor.setText(oferta.getAutor());
+        descricao.setText(oferta.getDescricao());
+        preco.setText(oferta.getPreco());
+        btnQueroIsto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Implementar funcionalidade de redirecionamento
+                // para sites ou serviço de Google Maps caso o seja endereço de loja fisica
+            }
+        });
+
+        return convertView;
+    }
+}
--- app/src/main/java/br/com/soaresdeveloper/tribarato/entidades/Oferta.java
+++ app/src/main/java/br/com/soaresdeveloper/tribarato/entidades/Oferta.java
@@ -6,10 +6,60 @@ package br.com.soaresdeveloper.tribarato.entidades;
 
 public class Oferta {
 
-    private String titulo;
     private String autor;
+    private String titulo;
     private String descricao;
-    private Double valor;
+    private String preco;
+    private String endereco;
 
+    public Oferta() {
+    }
 
+    public Oferta(String autor, String titulo, String descricao, String preco, String endereco) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.endereco = endereco;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
