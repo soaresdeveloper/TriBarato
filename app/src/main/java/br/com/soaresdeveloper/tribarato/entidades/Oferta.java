db8d976017cb6d929d81a7c3077020f078635a5f
soaresdeveloper <lucas3designer@gmail.com>
2017-12-14 16:54:55 -0200

    Adiciona layout item oferta

---
+++ app/src/main/java/br/com/soaresdeveloper/tribarato/entidades/Oferta.java
@@ -0,0 +1,15 @@
+package br.com.soaresdeveloper.tribarato.entidades;
+
+/**
+ * Created by soares on 14/12/17.
+ */
+
+public class Oferta {
+
+    private String titulo;
+    private String autor;
+    private String descricao;
+    private Double valor;
+
+
+}
